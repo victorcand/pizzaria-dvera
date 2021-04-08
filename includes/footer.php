46c1ab83b1c1de37de70f72691441322aef860d9
victor <victorcand@gmail.com>
2021-04-08 16:53:48 -0300

    atualização

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,3 @@
-    <footer class="rodape">
-        <p>Desenvolvido por Victor <?=date('Y')?></p>
-    </footer>
+
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,37 @@
     <link rel="stylesheet" href="public/css/all.css">
     <title>D'Vera Pizzaria</title>
 </head>
+
 <body>
+
     <header class="cabecalho">
-    
+        <div class="container-nav">
+            <div>
+                <a href="index.php" class="logo"><img src="public/images/logo-pizzaria.png"></a>
+            </div>
+
+            <nav class="menu-center">
+                <a href="#"><button class="btn-center">Peça Online</button></a>
+                <a href="#"><button class="btn-center">Cardápio</button></a>
+                <a href="#"><button class="btn-center">Promoções</button></a>
+                <a href="#"><button class="btn-center">Cadastrar Pizza</button></a>
+            </nav>
+
+            <nav class="menu-login">
+
+                <i class="far fa-user"></i>
+                <a href="#" class="btn-login"> Fazer Login</a>
+
+            </nav>
+
+            <nav class="menu-center">
+                <div class="menu-cart">
+
+                    <a href="#">
+                        <i class="fas fa-shopping-cart"></i>
+                        <p> Carrinho</p>
+                    </a>
+                </div>
+
+            </nav>
     </header>
